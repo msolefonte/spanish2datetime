86cd83dcce00e23c2b6506f8d2ef3d566abaca56
Wolfy <marcsole@insomniacwolves.com>
2018-05-15 21:45:31 +0200

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='spanish2datetime',
+    version='1.0',
+    description='Python3 script able to turn spanish text into datetime.',
+    author="Marc Solé",
+    author_email="marcsole@insomniacwolves.com",
+    license='LICENSE',
+    packages=['spanish2datetime']
+)
